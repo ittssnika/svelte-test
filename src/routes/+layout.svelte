0810b3e50537d39181c09e325a697aa6d7f1ef6a
ittssnika <nika2015.ru@gmail.com>
2024-03-09 22:26:04 +0500

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,11 @@
     <div class="nav-container">
         <a href="/" class ="nav-logo" title ="Back to Homepage">Вероника Прыгунова</a>
         <div class="nav-links">
-            <a href="/about" class="link">Обо мне</a>
-            <a href="/blog" class="link">Блог</a>
-            <a href="/project" class="link">Портфолио</a>
-            <a href="/contact" class="link">Контакты</a>
+            {#each nav as link}
+
+            <a href={link.href} class="link">{link.title}</a>
+
+            {/each}
         </div>
     </div>
 </nav>
@@ -14,7 +15,37 @@
     <!-- Pages will be injected below-->
     <slot></slot>
 </div>
+<script>
+    const nav =[
+    {
+    title: 'Обо мне',
+    href: '/about'
+    },
+    
+    {
+    title: 'Блог',
+    
+    href: '/blog'
+    
+    },
+    
+    {
+    title: 'Портфолио',
+    href: '/project'
+    },
 
+    {
+    title: 'Контакты',
+    href: '/contact'
+    },
+
+    {
+    title: 'temp',
+    href: '/temp'
+    }
+    
+    ]
+    </script>
 <style>
     .container {
         max-width: 1400px;
