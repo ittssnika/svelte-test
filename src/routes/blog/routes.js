5906ff66f472bcc07f27d4991b1343bfcc0c8948
ittssnika <nika2015.ru@gmail.com>
2024-03-10 14:21:22 +0500

    add blog

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -10,4 +10,16 @@ export const routes = [
     
     },
     
+
+
+    {
+
+        id: 2,
+        
+        title: 'My Second Blog Post',
+        
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
+        
+        },
+
     ];
